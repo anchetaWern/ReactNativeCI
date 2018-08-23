4bbd9a2ee0d19e79ecb992064beff0eaae81df5c
Wern Ancheta <ancheta.wern@gmail.com>
2018-08-23 21:27:51 +0800

    remove appcenter

--- appcenter-pre-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "MY CUSTOM PRE-BUILD SCRIPT..."
-if($UPDATE_JEST_SNAPSHOTS == "true")
-then
-  echo "Now updating the snapshots..."
-  npm test -u
-  echo "Done updating the snapshots"
-fi
